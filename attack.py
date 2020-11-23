d501b288b2968cf2b0b1a76c558787ee7a926667
Ara Software <71103842+cr4ck3r1@users.noreply.github.com>
2020-11-23 13:26:33 +0300

    Update attack.py

--- attack.py
+++ attack.py
@@ -1,24 +1,3 @@
-import uuid
-import requests
-import time
-import os
-
-c = input("nawt chia? ")
-    
-e = str(uuid.uuid5(uuid.NAMESPACE_DNS, c))
-print("")
-print(e)
-g = requests.get("https://pastebin.com/2WuEPjdc")
-
-if e in g.text:
-    print("active")
-    time.sleep(2)
-    from Vddos import attack
-    attack()
-    
-else:
-    print("id'akat active nia ")
-    print("")
-    print("nama bo instakam bnera")
-    print("")
-    print(" instagram: ara_software")
+from Vddos import attack
+attack()
+   
